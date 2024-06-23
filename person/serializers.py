7dbdbb4c39215cd83dd0235c390aab1f5e995e7f
Xushnoza <dellasmithxo07@gmail.com>
2024-06-23 12:27:55 +0500

    done

--- person/serializers.py
+++ person/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
+from .models import Person
 
-from person.models import Person
+
+class PoetSerializers(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    create_at = serializers.DateTimeField(read_only=True)
+    update_at = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
 
 
-class PoetSerializers(serializers.ModelSerializer):
-    class Meta :
-        model = Person
-        fields = ('name', "content")
+    def create(self, validated_data):
+        return Person.objects.create(**validated_data)
--- person/views.py
+++ person/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
 from .models import Person
 from .serializers import PoetSerializers
-class ListPoet(generics.ListAPIView):
-    queryset = Person.objects.all()
-    serializer_class = PoetSerializers
+class ListPoet(APIView):
+    def get(self, request):
+        lst = Person.objects.all().values()
+        return Response({'Poet': PoetSerializers(lst, many=True).data})
+
+    def post(self,requests):
+        serializers = PoetSerializers(data=requests.data)
+        serializers.is_valid(raise_exception=True)
+        posts = Person.objects.create(
+            name=requests.data["name"],
+            content=requests.data["content"],
+            cat_id=requests.data["cat_id"],
+        )
+        return Response({"posts": PoetSerializers(posts).data})
+
+
+# class ListPoet(generics.ListAPIView):
+#     queryset = Person.objects.all()
+#     serializer_class = PoetSerializers
